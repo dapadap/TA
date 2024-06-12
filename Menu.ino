8203ea2b86bdd91c0dd7a55bcf40d4d081e809b9
dapadap <100177549+dapadap@users.noreply.github.com>
2024-06-12 20:29:43 +0700

    Add files via upload

---
+++ Menu.ino
@@ -0,0 +1,158 @@
+void menu(){
+  DateTime now = rtc.now();
+  jam     = now.hour();
+  menit   = now.minute();
+  detik   = now.second();
+  tanggal = now.day();
+  bulan   = now.month();
+  tahun   = now.year();
+  hari    = daysOfTheWeek[now.dayOfTheWeek()];
+
+if(digitalRead(sett)==LOW){
+opsi++;
+delay(100);
+}if(opsi==8){
+  opsi=1;
+}
+
+switch(opsi){
+  case 1:
+  lcd2.setCursor(16,1);lcd2.print("Set");
+  lcd2.setCursor(15,2);lcd2.print("Point");
+  lcd2.setCursor(1,0);lcd2.print("t_max : ");lcd2.print(t_set_max);lcd2.print("C");
+  lcd2.setCursor(1,1);lcd2.print("t_min : ");lcd2.print(t_set_min);lcd2.print("C");
+  lcd2.setCursor(1,2);lcd2.print("h_max : ");lcd2.print(h_set_max);lcd2.print("%");
+  lcd2.setCursor(1,3);lcd2.print("h_min : ");lcd2.print(h_set_min);lcd2.print("%");
+  lcd2.setCursor(13,3); lcd2.print(" ");
+  lcd2.setCursor(13,0); lcd2.print("<");
+  if(digitalRead(up)==LOW){
+    t_set_max++;
+    delay(100);
+  }
+    if(digitalRead(down)==LOW){
+    t_set_max--;
+    delay(100);
+  }
+  break;
+
+  case 2:
+  lcd2.setCursor(16,1);lcd2.print("Set");
+  lcd2.setCursor(15,2);lcd2.print("Point");
+  lcd2.setCursor(1,0);lcd2.print("t_max : ");lcd2.print(t_set_max);lcd2.print("C");
+  lcd2.setCursor(1,1);lcd2.print("t_min : ");lcd2.print(t_set_min);lcd2.print("C");
+  lcd2.setCursor(1,2);lcd2.print("h_max : ");lcd2.print(h_set_max);lcd2.print("%");
+  lcd2.setCursor(1,3);lcd2.print("h_min : ");lcd2.print(h_set_min);lcd2.print("%");  
+  lcd2.setCursor(13,0); lcd2.print(" ");
+  lcd2.setCursor(13,1); lcd2.print("<");
+    if(digitalRead(up)==LOW){
+    t_set_min++;
+    delay(100);
+  }
+    if(digitalRead(down)==LOW){
+    t_set_min--;
+    delay(100);
+  }
+  break;
+
+  case 3:
+  lcd2.setCursor(16,1);lcd2.print("Set");
+  lcd2.setCursor(15,2);lcd2.print("Point");
+  lcd2.setCursor(1,0);lcd2.print("t_max : ");lcd2.print(t_set_max);lcd2.print("C");
+  lcd2.setCursor(1,1);lcd2.print("t_min : ");lcd2.print(t_set_min);lcd2.print("C");
+  lcd2.setCursor(1,2);lcd2.print("h_max : ");lcd2.print(h_set_max);lcd2.print("%");
+  lcd2.setCursor(1,3);lcd2.print("h_min : ");lcd2.print(h_set_min);lcd2.print("%");
+  lcd2.setCursor(13,1); lcd2.print(" ");
+  lcd2.setCursor(13,2); lcd2.print("<");
+    if(digitalRead(up)==LOW){
+    h_set_max++;
+    delay(100);
+  }
+    if(digitalRead(down)==LOW){
+    h_set_max--;
+    delay(100);
+  }
+  break;
+
+  case 4:
+  lcd2.setCursor(16,1);lcd2.print("Set");
+  lcd2.setCursor(15,2);lcd2.print("Point");
+  lcd2.setCursor(1,0);lcd2.print("t_max : ");lcd2.print(t_set_max);lcd2.print("C");
+  lcd2.setCursor(1,1);lcd2.print("t_min : ");lcd2.print(t_set_min);lcd2.print("C");
+  lcd2.setCursor(1,2);lcd2.print("h_max : ");lcd2.print(h_set_max);lcd2.print("%");
+  lcd2.setCursor(1,3);lcd2.print("h_min : ");lcd2.print(h_set_min);lcd2.print("%");
+  lcd2.setCursor(13,3); lcd2.print("<");
+  lcd2.setCursor(13,2); lcd2.print(" ");
+    if(digitalRead(up)==LOW){
+    h_set_min++;
+    delay(50);
+  }
+    if(digitalRead(down)==LOW){
+    h_set_min--;
+    delay(50);
+  }
+  break;
+
+  case 5:
+  lcd2.setCursor(16,1);lcd2.print("Set");
+  lcd2.setCursor(14,2);lcd2.print("Sprayer");
+  lcd2.setCursor(1,0);lcd2.print("Day    : ");lcd2.print(set_day);
+  lcd2.setCursor(1,1);lcd2.print("hour   : ");lcd2.print(set_hour);
+  lcd2.setCursor(1,2);lcd2.print("minute : ");lcd2.print(set_minute);
+  lcd2.setCursor(1,3);lcd2.print(jam);lcd2.print(":");lcd2.print(menit);lcd2.print(":");lcd2.print(detik);lcd2.print(" ");
+  lcd2.print(tanggal);lcd2.print("/");lcd2.print(bulan);lcd2.print("/");lcd2.print(tahun);lcd2.print("");
+  lcd2.setCursor(13,0); lcd2.print("<");
+  lcd2.setCursor(13,3); lcd2.print(" ");
+    if(digitalRead(up)==LOW){
+    set_day++;
+    delay(50);
+  }
+    if(digitalRead(down)==LOW){
+    set_day--;
+    delay(50);
+  }
+  break;
+
+  case 6:
+  lcd2.setCursor(16,1);lcd2.print("Set");
+  lcd2.setCursor(14,2);lcd2.print("Sprayer");
+  lcd2.setCursor(1,0);lcd2.print("Day    : ");lcd2.print(set_day);
+  lcd2.setCursor(1,1);lcd2.print("hour   : ");lcd2.print(set_hour);
+  lcd2.setCursor(1,2);lcd2.print("minute : ");lcd2.print(set_minute);
+  lcd2.setCursor(1,3);lcd2.print(jam);lcd2.print(":");lcd2.print(menit);lcd2.print(":");lcd2.print(detik);lcd2.print(" ");
+  lcd2.print(tanggal);lcd2.print("/");lcd2.print(bulan);lcd2.print("/");lcd2.print(tahun);lcd2.print("/");
+  lcd2.setCursor(13,1); lcd2.print("<");
+  lcd2.setCursor(13,0); lcd2.print(" ");
+    if(digitalRead(up)==LOW){
+    set_hour++;
+    delay(50);
+  }
+    if(digitalRead(down)==LOW){
+    set_hour--;
+    delay(50);
+  }
+  break;
+
+  case 7:
+  lcd2.setCursor(16,1);lcd2.print("Set");
+  lcd2.setCursor(14,2);lcd2.print("Sprayer");
+  lcd2.setCursor(1,0);lcd2.print("Day    : ");lcd2.print(set_day);
+  lcd2.setCursor(1,1);lcd2.print("hour   : ");lcd2.print(set_hour);
+  lcd2.setCursor(1,2);lcd2.print("minute : ");lcd2.print(set_minute);
+  lcd2.setCursor(1,3);lcd2.print(jam);lcd2.print(":");lcd2.print(menit);lcd2.print(":");lcd2.print(detik);lcd2.print(" ");
+  lcd2.print(tanggal);lcd2.print("/");lcd2.print(bulan);lcd2.print("/");lcd2.print(tahun);lcd2.print("/");
+  lcd2.setCursor(13,2); lcd2.print("<");
+  lcd2.setCursor(13,1); lcd2.print(" ");
+    if(digitalRead(up)==LOW){
+    set_minute++;
+    delay(50);
+  }
+    if(digitalRead(down)==LOW){
+    set_minute--;
+    delay(50);
+  }
+  break;
+  
+
+}
+
+}
