087f19f720bd8cbe3bdabb50e2f70ded9494e18f
dapadap <100177549+dapadap@users.noreply.github.com>
2024-06-13 19:02:30 +0700

    Update Menu.ino

--- Menu.ino
+++ Menu.ino
@@ -1,15 +1,9 @@
 void menu(){
-  DateTime now = rtc.now();
-  jam     = now.hour();
-  menit   = now.minute();
-  detik   = now.second();
-  tanggal = now.day();
-  bulan   = now.month();
-  tahun   = now.year();
-  hari    = daysOfTheWeek[now.dayOfTheWeek()];
-
 if(digitalRead(sett)==LOW){
 opsi++;
+delay(100);
+}if(digitalRead(avai1)==LOW){
+opsi--;
 delay(100);
 }if(opsi==8){
   opsi=1;
@@ -27,10 +21,12 @@ switch(opsi){
   lcd2.setCursor(13,0); lcd2.print("<");
   if(digitalRead(up)==LOW){
     t_set_max++;
+    EEPROM.write(1,t_set_max);
     delay(100);
   }
     if(digitalRead(down)==LOW){
     t_set_max--;
+    EEPROM.write(1,t_set_max);
     delay(100);
   }
   break;
@@ -46,10 +42,12 @@ switch(opsi){
   lcd2.setCursor(13,1); lcd2.print("<");
     if(digitalRead(up)==LOW){
     t_set_min++;
+    EEPROM.write(2,t_set_min);
     delay(100);
   }
     if(digitalRead(down)==LOW){
     t_set_min--;
+    EEPROM.write(2,t_set_min);
     delay(100);
   }
   break;
@@ -65,10 +63,12 @@ switch(opsi){
   lcd2.setCursor(13,2); lcd2.print("<");
     if(digitalRead(up)==LOW){
     h_set_max++;
+    EEPROM.write(3,h_set_max);
     delay(100);
   }
     if(digitalRead(down)==LOW){
     h_set_max--;
+    EEPROM.write(3,h_set_max);
     delay(100);
   }
   break;
@@ -84,70 +84,69 @@ switch(opsi){
   lcd2.setCursor(13,2); lcd2.print(" ");
     if(digitalRead(up)==LOW){
     h_set_min++;
+    EEPROM.write(4,h_set_min);
     delay(50);
   }
     if(digitalRead(down)==LOW){
     h_set_min--;
+    EEPROM.write(4,h_set_min);
     delay(50);
   }
   break;
 
   case 5:
-  lcd2.setCursor(16,1);lcd2.print("Set");
-  lcd2.setCursor(14,2);lcd2.print("Sprayer");
-  lcd2.setCursor(1,0);lcd2.print("Day    : ");lcd2.print(set_day);
-  lcd2.setCursor(1,1);lcd2.print("hour   : ");lcd2.print(set_hour);
-  lcd2.setCursor(1,2);lcd2.print("minute : ");lcd2.print(set_minute);
-  lcd2.setCursor(1,3);lcd2.print(jam);lcd2.print(":");lcd2.print(menit);lcd2.print(":");lcd2.print(detik);lcd2.print(" ");
+  lcd2.setCursor(1,0);lcd2.print("Day      : ");lcd2.print(day);
+  lcd2.setCursor(1,1);lcd2.print(jam);lcd2.print(":");lcd2.print(menit);lcd2.print(":");lcd2.print(detik);lcd2.print(" ");
   lcd2.print(tanggal);lcd2.print("/");lcd2.print(bulan);lcd2.print("/");lcd2.print(tahun);lcd2.print("");
-  lcd2.setCursor(13,0); lcd2.print("<");
-  lcd2.setCursor(13,3); lcd2.print(" ");
+  lcd2.setCursor(1,2);lcd2.print("Set Day  : ");lcd2.print(set_day);lcd2.print("       ");
+  lcd2.setCursor(1,3);lcd2.print("Set Clock: ");lcd2.print(set_hour);lcd2.print(":");lcd2.print(set_minute);
+
     if(digitalRead(up)==LOW){
     set_day++;
+    EEPROM.write(5,set_day);
     delay(50);
   }
     if(digitalRead(down)==LOW){
+    EEPROM.write(5,set_day);
     set_day--;
     delay(50);
   }
   break;
 
   case 6:
-  lcd2.setCursor(16,1);lcd2.print("Set");
-  lcd2.setCursor(14,2);lcd2.print("Sprayer");
-  lcd2.setCursor(1,0);lcd2.print("Day    : ");lcd2.print(set_day);
-  lcd2.setCursor(1,1);lcd2.print("hour   : ");lcd2.print(set_hour);
-  lcd2.setCursor(1,2);lcd2.print("minute : ");lcd2.print(set_minute);
-  lcd2.setCursor(1,3);lcd2.print(jam);lcd2.print(":");lcd2.print(menit);lcd2.print(":");lcd2.print(detik);lcd2.print(" ");
-  lcd2.print(tanggal);lcd2.print("/");lcd2.print(bulan);lcd2.print("/");lcd2.print(tahun);lcd2.print("/");
-  lcd2.setCursor(13,1); lcd2.print("<");
-  lcd2.setCursor(13,0); lcd2.print(" ");
+  lcd2.setCursor(1,0);lcd2.print("Day      : ");lcd2.print(day);
+  lcd2.setCursor(1,1);lcd2.print(jam);lcd2.print(":");lcd2.print(menit);lcd2.print(":");lcd2.print(detik);lcd2.print(" ");
+  lcd2.print(tanggal);lcd2.print("/");lcd2.print(bulan);lcd2.print("/");lcd2.print(tahun);lcd2.print("");
+  lcd2.setCursor(1,2);lcd2.print("Set Day  : ");lcd2.print(set_day);lcd2.print("       ");
+  lcd2.setCursor(1,3);lcd2.print("Set Clock: ");lcd2.print(set_hour);lcd2.print(":");lcd2.print(set_minute);
+
     if(digitalRead(up)==LOW){
     set_hour++;
+    EEPROM.write(6,set_hour);
     delay(50);
   }
     if(digitalRead(down)==LOW){
     set_hour--;
+    EEPROM.write(6,set_hour);
     delay(50);
   }
   break;
 
   case 7:
-  lcd2.setCursor(16,1);lcd2.print("Set");
-  lcd2.setCursor(14,2);lcd2.print("Sprayer");
-  lcd2.setCursor(1,0);lcd2.print("Day    : ");lcd2.print(set_day);
-  lcd2.setCursor(1,1);lcd2.print("hour   : ");lcd2.print(set_hour);
-  lcd2.setCursor(1,2);lcd2.print("minute : ");lcd2.print(set_minute);
-  lcd2.setCursor(1,3);lcd2.print(jam);lcd2.print(":");lcd2.print(menit);lcd2.print(":");lcd2.print(detik);lcd2.print(" ");
-  lcd2.print(tanggal);lcd2.print("/");lcd2.print(bulan);lcd2.print("/");lcd2.print(tahun);lcd2.print("/");
-  lcd2.setCursor(13,2); lcd2.print("<");
-  lcd2.setCursor(13,1); lcd2.print(" ");
+  lcd2.setCursor(1,0);lcd2.print("Day      : ");lcd2.print(day);
+  lcd2.setCursor(1,1);lcd2.print(jam);lcd2.print(":");lcd2.print(menit);lcd2.print(":");lcd2.print(detik);lcd2.print(" ");
+  lcd2.print(tanggal);lcd2.print("/");lcd2.print(bulan);lcd2.print("/");lcd2.print(tahun);lcd2.print("");
+  lcd2.setCursor(1,2);lcd2.print("Set Day  : ");lcd2.print(set_day);lcd2.print("       ");
+  lcd2.setCursor(1,3);lcd2.print("Set Clock: ");lcd2.print(set_hour);lcd2.print(":");lcd2.print(set_minute);
+
     if(digitalRead(up)==LOW){
     set_minute++;
+    EEPROM.write(7,set_minute);
     delay(50);
   }
     if(digitalRead(down)==LOW){
     set_minute--;
+    EEPROM.write(7,set_minute);
     delay(50);
   }
   break;
