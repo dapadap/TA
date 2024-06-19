dc2731171aa0b568174b3197711377ad03261a3b
dapadap <100177549+dapadap@users.noreply.github.com>
2024-06-19 13:36:04 +0700

    Update kirim_data.ino

--- kirim_data.ino
+++ kirim_data.ino
@@ -5,33 +5,28 @@ waktu_sekarang = millis();
 if(waktu_sekarang-waktu_sebelum>=5000){
 status = !status;
 if(status){
-Serial.println("kirim");
-//Serial2.println("#2 ampare#38,5#sha#123#220 kwH");  
+//Serial.println("kirim");
  Serial2.print("@");
- Serial2.print(t_set_max);Serial2.print("@");
- Serial2.print(t_set_min);Serial2.print("@");
- Serial2.print(h_set_max);Serial2.print("@");
- Serial2.print(h_set_min);Serial2.print("@");
- Serial2.print(t[0]);Serial2.print("@");
- Serial2.print(h[0]);Serial2.print("@");
- Serial2.print(t[1]);Serial2.print("@");
- Serial2.print(h[1]);Serial2.print("@");
- Serial2.print(t[2]);Serial2.print("@");
- Serial2.print(h[2]);Serial2.print("@");
- Serial2.print(t[3]);Serial2.print("@");   
- Serial2.print(h[3]);Serial2.print("@");
- Serial2.print(voltage1);Serial2.print("@");
- Serial2.print(current1);Serial2.print("@");
- Serial2.print(power1);Serial2.print("@");
- //Serial2.print(energy1);Serial2.print("@");
-// Serial2.print(frequency1);Serial2.print("@");
- //Serial2.print(pf1);Serial2.print("@");
- Serial2.print(voltage2);Serial2.print("@");  
- Serial2.print(current2);Serial2.print("@");
- Serial2.print(power2);Serial2.print("@");
- //Serial2.print(energy2);Serial2.print("@");
-// Serial2.print(frequency2);Serial2.print("@");
-// Serial2.print(pf2);
+ Serial2.print(t_set_max);Serial2.print("@");//1
+ Serial2.print(t_set_min);Serial2.print("@");//2
+ Serial2.print(h_set_max);Serial2.print("@");//3
+ Serial2.print(h_set_min);Serial2.print("@");//4
+ Serial2.print((t[0]+t[1])/2);Serial2.print("@");//5
+ Serial2.print((h[0]+h[1])/2);Serial2.print("@");//6
+ Serial2.print((t[2]+t[3])/2);Serial2.print("@");//7
+ Serial2.print((h[2]+h[3])/2);Serial2.print("@");//8/
+ Serial2.print(day);Serial2.print("@");//9
+ Serial2.print(set_day);Serial2.print("@");//10
+ Serial2.print(set_hour);Serial2.print("@");//11   
+ Serial2.print(set_minute);Serial2.print("@");//12
+ Serial2.print(voltage1);Serial2.print("@");//13
+ Serial2.print(current1);Serial2.print("@");//14
+ Serial2.print(energy1);Serial2.print("@");//15
+ Serial2.print(voltage2);Serial2.print("@");  //16
+ Serial2.print(current2);Serial2.print("@");//17
+ Serial2.print(energy2);Serial2.print("@");//18
+ Serial2.print(day);Serial2.print("@");//19
+
  
 }else{
 
